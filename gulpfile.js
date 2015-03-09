f5a03e614d99edd30d8b41e19ecb519ff5bf50ef
Russell Matney <russell.matney@gmail.com>
2015-03-08 23:31:17 -0400

    WIP upload function

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var zip = require('gulp-zip');
 var del = require('del');
 var install = require('gulp-install');
 var runSequence = require('run-sequence');
+var AWS = require('aws-sdk');
+var fs = require('fs');
 
 gulp.task('clean', function(cb) {
   del(['./dist', './dist.zip'], cb);
@@ -35,6 +38,48 @@ gulp.task('zip', function(callback) {
   );
 });
 
+gulp.task('upload', function() {
+
+  //TODO: pull from gulp-env
+  AWS.config.region = 'us-east-1';
+  var lambda = new AWS.Lambda();
+  var functionName = 'resizeImage';
+
+  lambda.getFunction({FunctionName: functionName}, function(err, data) {
+    if (err) {
+      if (err.statusCode === 404) {
+        var warning = 'Unable to find lambda function ' + deploy_function + '. '
+        warning += 'Verify the lambda function name and AWS region are correct.'
+        gutil.log(warning);
+      } else {
+        var warning = 'AWS API request failed. '
+        warning += 'Check your AWS credentials and permissions.'
+        gutil.log(warning);
+      }
+    }
+
+    var current = data.Configuration;
+    //TODO: pull from config/gulp-env
+    var params = {
+      FunctionName: functionName,
+      Handler: current.Handler,
+      Mode: current.Mode,
+      Role: current.Role,
+      Runtime: current.Runtime
+    };
+
+    fs.readFile('./dist.zip', function(err, data) {
+      params['FunctionZip'] = data;
+      lambda.uploadFunction(params, function(err, data) {
+        if (err) {
+          var warning = 'Package upload failed. '
+          warning += 'Check your iam:PassRole permissions.'
+          gutil.log(warning);
+        }
+      });
+    });
+  });
+});
 
 gulp.task('default', function() {
   console.log('sup, default task');
