72635bd0574dbea7c596e3092483e825cd52fe63
Russell Matney <russell.matney@gmail.com>
2015-03-08 20:28:02 -0400

    gulp zip workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,43 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
+var del = require('del');
+var install = require('gulp-install');
+var runSequence = require('run-sequence');
 
-gulp.task('zip', function() {
-  console.log('zippy');
+gulp.task('clean', function(cb) {
+  del('./dist',
+    del('./archive.zip', cb)
+  );
 });
 
-gulp.task('default', ['zip']);
+gulp.task('js', function() {
+  gulp.src('index.js')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('package-npm-mods', function() {
+  gulp.src('./package.json')
+    .pipe(gulp.dest('./dist/'))
+    .pipe(install({production: true}));
+});
+
+gulp.task('zip-it-up', function() {
+  gulp.src(['dist/**/*', '!dist/package.json'])
+    .pipe(zip('dist.zip'))
+    .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('zip', function(callback) {
+  return runSequence(
+    ['clean'],
+    ['js', 'package-npm-mods'],
+    ['zip-it-up'],
+    callback
+  );
+});
+
+
+gulp.task('default', function() {
+  console.log('sup, default task');
+});
