3681ffe61016313ca28717ec8765317ab531dc8e
Russell Matney <russell.matney@gmail.com>
2015-03-08 20:06:26 -0400

    zippy

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var zip = require('gulp-zip');
 
-gulp.task('default', function() {
-  console.log('howdy');
+gulp.task('zip', function() {
+  console.log('zippy');
 });
+
+gulp.task('default', ['zip']);
