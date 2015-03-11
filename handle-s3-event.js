7d77f16dbdd9492d3cb5981cabac8be5c72e9a50
Russell Matney <russell.matney@gmail.com>
2015-03-11 18:37:18 -0400

    handle-s3-event fully tested

--- handle-s3-event.js
+++ handle-s3-event.js
@@ -3,7 +3,26 @@ q = require('q')
 module.exports = function(event) {
   var s3Event = event;
   return function() {
+    var def = q.defer()
 
-    return event;
+    if (s3Event &&
+        s3Event.Records &&
+        s3Event.Records[0] &&
+        s3Event.Records[0].s3 &&
+        s3Event.Records[0].s3.bucket &&
+        s3Event.Records[0].s3.bucket.name &&
+        s3Event.Records[0].s3.object &&
+        s3Event.Records[0].s3.object.key) {
+
+      def.resolve({
+        srcBucket: s3Event.Records[0].s3.bucket.name,
+        srcKey: s3Event.Records[0].s3.object.key,
+      });
+
+    } else {
+      def.reject(new Error('Passed event could not be handled: ', s3Event))
+    }
+
+    return def.promise;
   }
 }
--- test/handle-s3-event.spec.js
+++ test/handle-s3-event.spec.js
@@ -11,4 +11,39 @@ describe('handleS3Event', function() {
     expect(handleS3Event()).to.be.a('Function');
   });
 
+  it('should return a promise', function(done) {
+    handleS3Event()().then(function() {
+      done()
+    }, function() {
+      done()
+    })
+  });
+
+  it('should throw an error if the input event is not as expected', function(done) {
+    var event = {}
+    var promise = handleS3Event(event)
+
+    promise().then(function() {
+      done(new Error('func should have failed in this test'))
+    }, function(err) {
+      expect(err).to.exist
+      done()
+    });
+  });
+
+  it('should return s3 data for the uploaded object', function(done) {
+    var event = require('./test-input')
+    var promise = handleS3Event(event)
+
+    promise().then(function(s3Data) {
+      if (!s3Data || !s3Data.srcBucket || !s3Data.srcKey) {
+        done(new Error("expected Bucket and Key to exist"))
+      } else {
+        done()
+      }
+    }, function(err) {
+      done(err)
+    });
+  });
+
 });
