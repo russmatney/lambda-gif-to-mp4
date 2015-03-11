06fd18df731c97a82831b74e785d6ae6d0a5851e
Russell Matney <russell.matney@gmail.com>
2015-03-11 17:06:33 -0400

    refactors for promises and consistent ffmpeg availability. hits local ffmpeg if NODE_ENV=testing

--- index.js
+++ index.js
@@ -1,129 +1,68 @@
 // dependencies
-var async = require('async');
 var AWS = require('aws-sdk');
 var gm = require('gm')
             .subClass({ imageMagick: true }); // Enable ImageMagick integration.
-var util = require('util');
+var ffmpeg = require('fluent-ffmpeg');
+var q = require('q');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT']
 
-require('child_process').exec(
-  'cp /var/task/ffmpeg /tmp/.; chmod 755 /tmp/ffmpeg',
-  function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-  }
-);
-
-var ffmpeg = require('fluent-ffmpeg');
-
-// constants
-var MAX_WIDTH  = 100;
-var MAX_HEIGHT = 100;
-
-// get reference to S3 client
 var s3 = new AWS.S3();
 
 exports.handler = function(event, context) {
 
-  console.log("PATH")
-  console.log(process.env['PATH'])
+  process.env['FFMPEG_PATH'] = '/tmp/ffmpeg';
+  process.env['FFPROBE_PATH'] = '/tmp/ffprobe';
 
-  process.env['FFMPEG_PATH'] = '/tmp/ffmpeg'
-  process.env['FFPROBE_PATH'] = '/tmp/ffprobe'
+  var moveAndChmodFfmpegBinary = function() {
+    var def = q.defer()
 
-  ffmpeg.getAvailableFormats(function(err, formats) {
-    console.log('error getting formats');
-    console.log(err);
-    console.log('Available formats:');
-    console.dir(formats);
-  });
-
-
-  // Read options from the event.
-  console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
-  var srcBucket = event.Records[0].s3.bucket.name;
-  var srcKey    = event.Records[0].s3.object.key;
-  var dstBucket = srcBucket + "-resized";
-  var dstKey    = "resized-" + srcKey;
-
-  // Sanity check: validate that source and destination are different buckets.
-  if (srcBucket == dstBucket) {
-    console.error("Destination bucket must not match source bucket.");
-    return;
-  }
-
-  // Infer the image type.
-  var typeMatch = srcKey.match(/\.([^.]*)$/);
-  if (!typeMatch) {
-    console.error('unable to infer image type for key ' + srcKey);
-    return;
-  }
-  var imageType = typeMatch[1];
-  if (imageType != "jpg" && imageType != "png") {
-    console.log('skipping non-image ' + srcKey);
-    return;
-  }
-
-  // Download the image from S3, transform, and upload to a different S3 bucket.
-  async.waterfall([
-    function download(next) {
-      // Download the image from S3 into a buffer.
-      s3.getObject({
-          Bucket: srcBucket,
-          Key: srcKey
-        },
-        next);
-      },
-    function tranform(response, next) {
-      gm(response.Body).size(function(err, size) {
-        // Infer the scaling factor to avoid stretching the image unnaturally.
-        var scalingFactor = Math.min(
-          MAX_WIDTH / size.width,
-          MAX_HEIGHT / size.height
-        );
-        var width  = scalingFactor * size.width;
-        var height = scalingFactor * size.height;
-
-        // Transform the image buffer in memory.
-        this.resize(width, height)
-          .toBuffer(imageType, function(err, buffer) {
-            if (err) {
-              next(err);
-            } else {
-              next(null, response.ContentType, buffer);
-            }
-          });
-      });
-    },
-    function upload(contentType, data, next) {
-      // Stream the transformed image to a different S3 bucket.
-      s3.putObject({
-          Bucket: dstBucket,
-          Key: dstKey,
-          Body: data,
-          ContentType: contentType
-        },
-        next);
+    require('child_process').exec(
+      'cp /var/task/ffmpeg /tmp/.; chmod 755 /tmp/ffmpeg',
+      function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+          def.reject(error)
+        } else {
+          def.resolve()
+        }
       }
-    ], function (err) {
+    )
+
+    return def.promise;
+  }
+
+  var printFormats = function() {
+    var def = q.defer()
+
+    ffmpeg.getAvailableFormats(function(err, formats) {
       if (err) {
-        console.error(
-          'Unable to resize ' + srcBucket + '/' + srcKey +
-          ' and upload to ' + dstBucket + '/' + dstKey +
-          ' due to an error: ' + err
-        );
+        console.log('error getting formats');
+        console.log(err);
+        def.reject(err);
       } else {
-        console.log(
-          'Successfully resized ' + srcBucket + '/' + srcKey +
-          ' and uploaded to ' + dstBucket + '/' + dstKey
-        );
+        console.log('Available formats:');
+        console.dir(formats.wtv);
+        def.resolve()
       }
+    });
 
-      context.done();
-    }
-  );
+    return def.promise;
+  }
+
+  var promises = [];
+
+  if (!process.env.NODE_ENV || process.env.NODE_ENV != 'testing') {
+    promises.push(moveAndChmodFfmpegBinary);
+  }
+
+  promises.push(printFormats);
+
+  console.log('promises');
+  console.log(promises);
+
+  promises.reduce(q.when, q());
+
 };
