104456d63cea69a849cf097ddba658e768259522
Russell Matney <russell.matney@gmail.com>
2015-03-12 19:28:56 -0400

    cleaner promises, starting to add functions back

--- index.js
+++ index.js
@@ -14,15 +14,18 @@ process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'
 
 var tmpPrefix;
 var pathToBash;
+var handleS3Event;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV != 'testing') {
   //production
   tmpPrefix = '/tmp/';
   pathToBash = '/tmp/bash-scrap';
+  handleS3Event = require('handle-s3-event');
 } else {
   //local
   tmpPrefix = './';
   pathToBash = './bin/bash-scrap';
+  handleS3Event = require('./local_modules/handle-s3-event');
 }
 
 var s3 = new AWS.S3();
@@ -34,48 +37,52 @@ exports.handler = function(event, context) {
   var promises = [];
 
   if (!process.env.NODE_ENV || process.env.NODE_ENV != 'testing') {
-    promises.push(function() {
-      var def = q.defer()
+    promises.push(q.Promise(function(resolve, reject, notify) {
 
       proc.exec(
         'cp /var/task/ffmpeg /tmp/.; chmod 755 /tmp/ffmpeg; cp /var/task/bash-scrap /tmp/.; chmod 755 /tmp/bash-scrap',
         function (error, stdout, stderr) {
           if (error) {
             console.log('error setting up bins');
-            def.reject(error)
+            reject(error)
           } else {
             console.log("moved, updated binaries");
-            def.resolve()
+            resolve()
           }
         }
       )
 
-      return def.promise;
-    })
+    }))
   }
 
-  promises.push(function() {
-    var def = q.defer()
+  promises.push(handleS3Event(event))
+  promises.push(function(options) {
+    return q.Promise(function(resolve, reject) {
+      console.log('fetching from s3')
+      console.log(options);
+      resolve(options);
 
+    })
+  });
+
+  promises.push(q.Promise(function(resolve, reject) {
     console.log('launching bash script');
 
     var child = proc.spawn(pathToBash);
     child.stdout.on('data', function (data) {
-      console.log("stdout:\n"+data);
+      console.log("stdout:\n" + data);
     });
     child.stderr.on('data', function (data) {
-      console.log("stderr:\n"+data);
-      def.reject(data);
+      console.log("stderr:\n" + data);
+      reject(data);
     });
     child.on('close', function (code) {
       console.log(code);
-      def.resolve();
+      resolve();
 
       context.done()
     });
-
-    return def.promise;
-  });
+  }));
 
   promises.reduce(q.when, q())
     .fail(function(err){
