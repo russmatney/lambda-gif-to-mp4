de6118f3e7a6c79a20cdb4cf5781149561784813
Russell Matney <russell.matney@gmail.com>
2015-03-13 13:09:03 -0400

    clears /tmp/, exposes stdout/stderr, shows uploadprogress

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var gm = require('gm')
-          .subClass({imageMagick: true})
-var mkdirp = require('mkdirp')
+          .subClass({imageMagick: true});
+var mkdirp = require('mkdirp');
+var zlib = require('zlib');
 
 var proc = require('child_process');
 
@@ -41,7 +42,7 @@ exports.handler = function(event, context) {
       return q.Promise(function(resolve, reject, notify) {
         console.log('manipulating binaries');
         proc.exec(
-          'cp /var/task/ffmpeg /tmp/.; chmod 755 /tmp/ffmpeg; cp /var/task/gif2mp4 /tmp/.; chmod 755 /tmp/gif2mp4',
+          'rm /tmp/*; cp /var/task/ffmpeg /tmp/.; chmod 755 /tmp/ffmpeg; cp /var/task/gif2mp4 /tmp/.; chmod 755 /tmp/gif2mp4;',
           function (error, stdout, stderr) {
             if (error) {
               reject(error)
@@ -80,8 +81,10 @@ exports.handler = function(event, context) {
 
       var child = proc.spawn(pathToBash, [options.gifPath]);
       child.stdout.on('data', function (data) {
+        console.log("stdout: " + data);
       });
       child.stderr.on('data', function (data) {
+        console.log("stderr: " + data);
       });
       child.on('exit', function (code) {
         if (code != 0) {
@@ -98,15 +101,18 @@ exports.handler = function(event, context) {
     console.log('ready for upload');
     options.mp4Path = '/tmp/' + path.basename(options.gifPath, '.gif') + '-final.mp4';
 
-    var stream = fs.createReadStream(options.mp4Path);
     var params = {
       Bucket: options.srcBucket + "-resized",
       Key: path.basename(options.mp4Path),
-      Body: stream,
       ContentType: mime.lookup(options.mp4Path)
-    };
+    }
 
-    s3.upload(params)
+    var body = fs.createReadStream(options.mp4Path)
+    var s3obj = new AWS.S3({params: params});
+    s3obj.upload({Body: body})
+      .on('httpUploadProgress', function(evt) {
+        console.log('upload progress: ' + (100 * evt.loaded / evt.total));
+      })
       .send(function(err, data) {
         if (err) {
           console.log('error');
