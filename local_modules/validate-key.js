ab00b2a1fc00bba17c3bc200afd8cc1c481c0128
Russell Matney <russell.matney@gmail.com>
2015-03-13 19:14:09 -0400

    implements validate-key modulegst

--- local_modules/validate-key.js
+++ local_modules/validate-key.js
@@ -3,9 +3,24 @@ q = require('q');
 module.exports = function(options) {
   return q.Promise(function(resolve, reject) {
 
-    if (!options || !options.srcKey) {
-      reject()
+    function endsWith(string, suffixPattern) {
+      return suffixPattern.test(string);
     }
 
+    function pathIs(str, prefix) {
+      return str.indexOf(prefix) == 0;
+    }
+
+    if (!options || !options.srcKey) {
+      reject(new Error("Options with .srcKey expected in validateKey"))
+    } else if(!pathIs(options.srcKey, 'events/')) {
+      reject(new Error("Uploaded file is not in events/ folder"))
+    } else if(!endsWith(options.srcKey, /\.gif$/)) {
+      reject(new Error("Uploaded file is not a .gif, exiting"))
+    } else if(endsWith(options.srcKey, /_\d+\.gif$/)) {
+      reject(new Error("Uploaded file is a _*.gif, exiting"))
+    } else {
+      resolve(options)
+    }
   })
 }
--- test/validate-key.spec.js
+++ test/validate-key.spec.js
@@ -32,4 +32,60 @@ describe.only('validateKey', function() {
       });
   })
 
+  it('should throw an error for non- "events/" keys', function(done) {
+    var options = {
+      srcKey: "jeff-goldblum.gif"
+    }
+
+    validateKey(options)
+      .then(function() {
+        done(new Error("validateKey should have rejected this key: " + options.srcKey))
+      }, function() {
+        done()
+      })
+  })
+
+  it('should throw an error for non .gifs', function(done) {
+    var options = {
+      srcKey: "events/jeff-goldblum.mp4"
+    }
+
+    validateKey(options)
+      .then(function() {
+        done(new Error("validateKey should have errored on an .mp4"))
+      }, function() {
+        done()
+      })
+
+  })
+
+  it('should throw an error for _*.gif files', function(done) {
+    var options = {
+      srcKey: "events/jeff-goldblum_180.gif"
+    }
+
+    validateKey(options)
+      .then(function() {
+        done(new Error("validateKey should have errored on an _180.gif"))
+      }, function() {
+        done()
+      })
+  });
+
+  it('should return the options object with solid keys', function(done) {
+    var options = {
+      srcKey: "events/jeff-goldblum.gif"
+    }
+    validateKey(options)
+      .then(function(resolved) {
+        if (!resolved || resolved !== options) {
+          done(new Error('ValidateKey should resolve options as handed in'))
+        } else {
+          done()
+        }
+      }, function() {
+        done(new Error("ValidateKey should have allowed this key: " + options.srcKey));
+      });
+  });
+
 });
