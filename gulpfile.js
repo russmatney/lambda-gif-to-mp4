56626720a402c359c6c7d20620c66510f8a4e3af
Russell Matney <russell.matney@gmail.com>
2015-03-16 16:35:41 -0400

    gulpfile and config clean up

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var runSequence = require('run-sequence');
 var AWS = require('aws-sdk');
 var fs = require('fs');
 var mocha = require('gulp-mocha');
+var env = require('gulp-env');
 
 gulp.task('clean', function(cb) {
   del(['./dist', './dist.zip'], cb);
@@ -28,114 +29,61 @@ gulp.task('package-npm-mods', function() {
     .pipe(install({production: true}));
 });
 
-gulp.task('zip-it-up', function() {
+gulp.task('zip', function() {
   return gulp.src(['dist/**/*', '!dist/package.json'])
     .pipe(zip('dist.zip'))
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('zip', function(callback) {
+gulp.task('lambda-zip', function(callback) {
   return runSequence(
     ['clean'],
     ['js', 'copy-binaries', 'package-npm-mods'],
-    ['zip-it-up'],
+    ['zip'],
     callback
   );
 });
 
-gulp.task('zip-and-upload', function(callback) {
+gulp.task('zipload', function(callback) {
   return runSequence(
-    ['clean'],
-    ['js', 'copy-binaries', 'package-npm-mods', 'local-modules'],
-    ['zip-it-up'],
+    ['lambda-zip'],
     ['upload'],
     callback
   );
 });
 
+gulp.task('upload', function(callback) {
+  env({ file: "lambda-config.js" });
 
-var config = {
-  region: 'us-east-1',
-  handler: 'index.handler',
-  role: 'arn:aws:iam::106586740595:role/executionrole',
-  functionName: 'gif-to-mp4-lambduh',
-  timeout: 30
-}
-
-//this func exits earlier than its process
-gulp.task('upload-new', function() {
-
-  AWS.config.region = config.region;
+  AWS.config.region = process.env.region;
   var lambda = new AWS.Lambda();
 
   var params = {
-    FunctionName: config.functionName,
-    Handler: config.handler,
+    FunctionName: process.env.functionName,
+    Handler: process.env.handler,
     Mode: "event",
-    Role: config.role,
+    Role: process.env.role,
     Runtime: "nodejs",
-    Timeout: config.timeout
+    Timeout: process.env.timeout
   };
 
-  fs.readFile('./dist.zip', function(err, data) {
+  return fs.readFile('./dist.zip', function(err, data) {
     params['FunctionZip'] = data;
     lambda.uploadFunction(params, function(err, data) {
       if (err) {
         var warning = 'Package upload failed. '
         warning += 'Check your iam:PassRole permissions.'
         gutil.log(warning);
+        callback(err)
       }
-    });
-  });
-});
-
-//this func exits earlier than its process
-gulp.task('upload', function() {
-
-  //TODO: pull from gulp-env
-  AWS.config.region = config.region;
-  var functionName = config.functionName;
-  var lambda = new AWS.Lambda();
-
-  lambda.getFunction({FunctionName: functionName}, function(err, data) {
-    if (err) {
-      if (err.statusCode === 404) {
-        var warning = 'Unable to find lambda function ' + functionName + '. '
-        warning += 'Verify the lambda function name and AWS region are correct.'
-        gutil.log(warning);
-      } else {
-        var warning = 'AWS API request failed. '
-        warning += 'Check your AWS credentials and permissions.'
-        gutil.log(warning);
-      }
-    }
-
-    var current = data.Configuration;
-    var params = {
-      FunctionName: functionName,
-      Handler: current.Handler,
-      Mode: current.Mode,
-      Role: current.Role,
-      Runtime: current.Runtime,
-      Timeout: config.timeout
-    };
-
-    fs.readFile('./dist.zip', function(err, data) {
-      params['FunctionZip'] = data;
-      lambda.uploadFunction(params, function(err, data) {
-        if (err) {
-          var warning = 'Package upload failed. '
-          warning += 'Check your iam:PassRole permissions.'
-          gutil.log(warning);
-        }
-      });
+      callback()
     });
   });
 });
 
 gulp.task('watch', function() {
   gulp.watch(
-    ['*.js', 'test/*.js', 'fixtures/*.js'],
+    ['*.js', 'test/*.js'],
     ['mocha']
   );
 });
---
+++ lambda-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  region: 'us-east-1',
+  handler: 'index.handler',
+  role: 'arn:aws:iam::106586740595:role/executionrole',
+  functionName: 'gif-to-mp4-lambduh-new-test',
+  timeout: 50
+}
+
+
